dd0948bf272868d46bd7dee9ac88e7d6d6ee2c28
Jeremy Kithome <jeremy.kithome@andela.com>
2018-06-29 11:09:04 +0300

    configure app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import Main from './components/main';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const store = configureStore();
 
+const App = () => (
+  <Provider store={store}>
+    <Main />
+  </Provider>
+);
 export default App;
